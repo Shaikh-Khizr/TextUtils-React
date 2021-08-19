bdf6ec59450a8eab923865fa0eb1cde292f3d5d0
Shaikh-Khizr <khizars949@gmail.com>
2021-08-19 12:00:49 +0530

    Video 12 completed

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -44,13 +44,12 @@ export default function TextForm(props) {
     const [text, setText] = useState('');
     // text = "new text"; // Wrong way to change the state
     // setText("new text"); // Correct way to change the state
-    // setText("")
     return (
         <>
-        <div className="container">
+        <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary mx-1" onClick={handleUpClick} >Conver to Uppercase</button>
             <button className="btn btn-primary mx-1" onClick={handleLoClick} >Conver to Lowercase</button>
@@ -59,12 +58,12 @@ export default function TextForm(props) {
             <button className="btn btn-primary mx-1" onClick={handleCopy} >Copy Text</button>
             <button className="btn btn-primary mx-1" onClick={handleExtraSpaces} >Remove Extra Spaces</button>
         </div>
-        <div className="container my-3">
+        <div className="container my-3" style={{color: props.mode==='dark'?'white':'$042743'}}>
             <h2>You text summary</h2>
             <p>{text.split(" ").length} words {text.length} characters</p>
             <p>{0.008 * text.split(" ").length} Minutes read</p>
             <h2>Preivew</h2>
-            <p>{text}</p>
+            <p>{text.length > 0 ? text : 'Enter something in the text box above to preview it here'}</p>
         </div>
         </>
     )
