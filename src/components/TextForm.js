0bf67f7033aa6aa567755063ce3bc18440d70dd6
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2021-08-24 19:02:43 +0530

    Video 17 Completed

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -57,7 +57,7 @@ export default function TextForm(props) {
 
     const getTextAreaBgColor = (mode) => {
         if (mode === 'dark') {
-            return 'grey';
+            return 'rgb(19, 70, 110)';
         } else if (mode === 'green') {
             return '#0b5b27';
         } else {
@@ -72,19 +72,19 @@ export default function TextForm(props) {
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: getTextAreaBgColor(props.mode), color: props.mode === 'light' ? '#042743'  : 'white'}} id="myBox" rows="8"></textarea>
             </div>
-            <button className={`btn btn-${btnColor} mx-1`} onClick={handleUpClick} >Conver to Uppercase</button>
-            <button className={`btn btn-${btnColor} mx-1`} onClick={handleLoClick} >Conver to Lowercase</button>
-            <button className={`btn btn-${btnColor} mx-1`} onClick={handleClearTextClick} >Clear Text</button>
-            <button className={`btn btn-${btnColor} mx-1`} onClick={handleRemoveWhiteSpacesClick} >Remove White Spaces</button>
-            <button className={`btn btn-${btnColor} mx-1`} onClick={handleCopy} >Copy Text</button>
-            <button className={`btn btn-${btnColor} mx-1`} onClick={handleExtraSpaces} >Remove Extra Spaces</button>
+            <button className={`btn btn-${btnColor} mx-1 my-1`} onClick={handleUpClick} >Conver to Uppercase</button>
+            <button className={`btn btn-${btnColor} mx-1 my-1`} onClick={handleLoClick} >Conver to Lowercase</button>
+            <button className={`btn btn-${btnColor} mx-1 my-1`} onClick={handleClearTextClick} >Clear Text</button>
+            <button className={`btn btn-${btnColor} mx-1 my-1`} onClick={handleRemoveWhiteSpacesClick} >Remove White Spaces</button>
+            <button className={`btn btn-${btnColor} mx-1 my-1`} onClick={handleCopy} >Copy Text</button>
+            <button className={`btn btn-${btnColor} mx-1 my-1`} onClick={handleExtraSpaces} >Remove Extra Spaces</button>
         </div>
         <div className="container my-3" style={{color: props.mode === 'light' ? '#042743'  : 'white'}}>
             <h2>You text summary</h2>
-            <p>{text.length > 0 ? text.trim().split(" ").length : 0} words {text.length} characters</p>
-            <p>{0.008 * text.split(" ").length} Minutes read</p>
+            <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words {text.trim().length} characters</p>
+            <p>{0.008 * text.trim().length} Minutes read</p>
             <h2>Preivew</h2>
-            <p>{text.length > 0 ? text : 'Enter something in the text box above to preview it here'}</p>
+            <p>{text.trim().length > 0 ? text : 'Enter something in the text box above to preview it here'}</p>
         </div>
         </>
     )
